04bff81c582362f335ff6dee588a7feaaed53a3f
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-09-25 19:36:00 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,22 @@
+--   _____  _   __        ___            __   _  ______                   __            __   __                    
+--  / ___/ / | / /       /   |   ____   / /_ (_)/ ____/____   ____ ___   / /_   ____ _ / /_ / /   ____   ____ _    
+--  \__ \ /  |/ /______ / /| |  / __ \ / __// // /    / __ \ / __ `__ \ / __ \ / __ `// __// /   / __ \ / __ `/    
+-- ___/ // /|  //_____// ___ | / / / // /_ / // /___ / /_/ // / / / / // /_/ // /_/ // /_ / /___/ /_/ // /_/ /     
+--/____//_/ |_/       /_/  |_|/_/ /_/ \__//_/ \____/ \____//_/ /_/ /_//_.___/ \__,_/ \__//_____/\____/ \__, /      
+--                                                                                                    /____/
+
+-----------------------------Config--------------------------------
+Config = {}
+Config.Discord = {
+    enable = true,
+    title = "ANTI COMBAT LOG",                  --Ueberschrift vom Embed
+    url = "https://discord.gg/WrQmADK3",        --Unser Server (Discord)
+    color =  15108368,                          --https://www.spycolor.com
+    text = "ANTI COMBAT - NEWS",                --Text
+    username = "ANTI COMBAT - NEWS",            --Username
+    icon_url = "",                              --Icon Logo
+    thumb_url = "",                             --Thumbnail
+    webhook = "",                               --Webhook for Discord 
+}
+-------------------------------------------------------------------
+-- (Bei ausfuehrung des Commands **/Testing** kommt ein Test Embed in den Log-channel)
